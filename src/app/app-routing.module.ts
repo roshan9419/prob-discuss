6c5ca2362d3ce219ad93770779fb19570dd37c8a
Roshan <roshank9419@gmail.com>
2021-10-21 15:04:43 +0530

    added routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddQuestionComponent } from './components/add-question/add-question.component';
+import { HomeQuestionsComponent } from './components/home-questions/home-questions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeQuestionsComponent },
+  { path: 'ask-question', component: AddQuestionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
