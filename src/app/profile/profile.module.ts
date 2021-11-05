706545c5777bd693c0a40e0bce6c19a9bd4b4788
Roshan <roshank9419@gmail.com>
2021-11-05 23:44:13 +0530

    profile header component removed + profile heading improved

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProfileHeaderComponent } from './profile-header/profile-header.component';
 import { ProfileComponent } from './profile.component';
 import { SharedModule } from '../shared/shared.module';
 
@@ -8,7 +7,6 @@ import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
-    ProfileHeaderComponent,
     ProfileComponent
   ],
   imports: [
@@ -16,7 +14,6 @@ import { SharedModule } from '../shared/shared.module';
     SharedModule
   ],
   exports: [
-    ProfileHeaderComponent
   ]
 })
 export class ProfileModule { }
