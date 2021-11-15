5f34d45c53e290fe2ae56878aac22b795a21aa72
Roshan <roshank9419@gmail.com>
2021-11-15 23:44:06 +0530

    optimized authService

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -9,28 +9,47 @@ import { DBService } from './db.service';
 })
 export class AuthService {
 
-  constructor(public auth: AngularFireAuth, private dbService: DBService) {
+  authState: firebase.User | null = null;
+
+  constructor(private afu: AngularFireAuth, private dbService: DBService) {
     console.log("AuthService Initialized");
+    this.afu.authState.subscribe((auth => {
+      this.authState = auth;
+    }))
   }
 
-  isAuthValidated = async () => {
-    const user = await this.auth.currentUser;
-    return user != null && user.uid != null;
+  get currentUserId(): string | null {
+    return (this.authState !== null) ? this.authState.uid : null;
   }
 
-  getUser = async () => await this.auth.currentUser;
+  get currentUserName(): string | null {
+    return (this.authState !== null) ? this.authState.displayName : null;
+  }
+
+  get currentUser() {
+    return this.authState;
+  }
+
+  get isAuthValidated(): boolean {
+    return this.authState != null;
+  }
 
   async googleLoginIn() {
-    const response = await this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-    const user = response.user!;
+    const response = await this.afu.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    if (!response || !response.user) {
+      throw new Error("Failed to login");
+    }
+
+    this.authState = response.user;
+
     // Check if account exists in DB or not
-    const isExist = await this.dbService.isUserExists(user.uid);
+    const isExist = await this.dbService.isUserExists(this.authState.uid);
     if (!isExist) {
       const appUser = new User();
-      appUser.userId = user.uid;
-      appUser.username = user.displayName!;
-      appUser.email = user.email;
-      appUser.photoUrl = user.photoURL!;
+      appUser.userId = this.authState.uid;
+      appUser.username = this.authState.displayName!;
+      appUser.email = this.authState.email;
+      appUser.photoUrl = this.authState.photoURL!;
       appUser.registeredDate = new Date();
 
       await this.dbService.updateUser(appUser);
@@ -38,7 +57,8 @@ export class AuthService {
     return response.user;
   }
 
-  async signOut() {
-    await this.auth.signOut();
+  async signOut(next: Function) {
+    await this.afu.signOut();
+    if (next) next();
   }
 }
