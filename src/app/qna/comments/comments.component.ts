21c6994afa143532e582c9317a511bc42919c681
Roshan <roshank9419@gmail.com>
2021-11-25 00:39:35 +0530

    added validation, ui improved in comments

--- src/app/qna/comments/comments.component.ts
+++ src/app/qna/comments/comments.component.ts
@@ -17,13 +17,16 @@ export class CommentsComponent implements OnInit {
 
   comments: QnAComment[] = [];
   inputComment: string = ''
+  
+  addCommentEnabled: boolean = false;
+  maxChar = 500
 
   constructor(private authService: AuthService, private dbService: DBService, private snackbarService: SnackbarService) { }
 
   ngOnInit(): void {
     this.fetchComments();
   }
-
+  
   async fetchComments() {
     this.comments = await this.dbService.fetchQnAComments(this.qnaId, this.isQuestionComment);
   }
@@ -33,16 +36,22 @@ export class CommentsComponent implements OnInit {
       this.snackbarService.showSnackbar("Login to comment", SnackbarType.INFO);
       return;
     }
-    if (!this.inputComment || this.inputComment.length < 10) return;
-    const comment = new QnAComment();
-    comment.content = this.inputComment;
-    comment.qnaId = this.qnaId;
-    comment.commentDate = new Date();
-    comment.userId = this.authService.userId!;
-    comment.username = this.authService.userName!;
-    this.dbService.addCommentOnQnA(comment, this.isQuestionComment);
-    this.comments.push(comment);
-    this.snackbarService.showSnackbar("Comment added", SnackbarType.SUCCESS);
+    if (!this.inputComment || this.inputComment.length < 100 || this.inputComment.length > this.maxChar) return;
+    try {
+      const comment = new QnAComment();
+      comment.content = this.inputComment;
+      comment.qnaId = this.qnaId;
+      comment.commentDate = new Date();
+      comment.userId = this.authService.userId!;
+      comment.username = this.authService.userName!;
+      this.dbService.addCommentOnQnA(comment, this.isQuestionComment);
+      this.comments.push(comment);
+      this.snackbarService.showSnackbar("Comment added", SnackbarType.SUCCESS);
+      this.addCommentEnabled = false;
+      this.inputComment = "";
+    } catch(e) {
+      this.snackbarService.showSnackbar("Something went wrong", SnackbarType.INFO);
+    }
   }
 
 }
