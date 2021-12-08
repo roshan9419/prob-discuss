497f70e410625f1cb42ccd4553c945d24614738d
Roshan <roshank9419@gmail.com>
2021-12-09 00:51:15 +0530

    fixed pagination issue

--- src/app/qna/home-questions/home-questions.component.ts
+++ src/app/qna/home-questions/home-questions.component.ts
@@ -22,18 +22,13 @@ export class HomeQuestionsComponent implements OnInit {
     this.activatedroute.queryParams.subscribe(
       params => {
         // console.log(params);
-        this.loadRecentQuestions(params.pageToken, params.f);
+        this.loadRecentQuestions(params.pageToken, !(params.f === 'false'));
       }
     );
   }
 
-  async loadRecentQuestions(pageToken: string, forward: boolean | null | undefined) {
+  async loadRecentQuestions(pageToken: string, forward: boolean) {
     try {
-
-      this.nextPageToken = null;
-      this.prevPageToken = null;
-
-      if (forward === undefined || forward === null) forward = true;
       const response = await this.dbService.fetchRecentQuestions(10, forward, pageToken);
       if (!response) {
         throw new Error("Something went wrong");
@@ -47,12 +42,6 @@ export class HomeQuestionsComponent implements OnInit {
       console.log(e);
       this.route.navigateByUrl('404');
     }
-  }
-
-  onPageBtnTap(isNext: boolean) {
-    this.isLoading = true;
-    if (isNext) this.loadRecentQuestions(this.nextPageToken!, true);
-    else this.loadRecentQuestions(this.prevPageToken!, false);
   }
 
 }
